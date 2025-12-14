b76e45b454a1883965af07b582ce4cf998a4b06f
raghav-upadhyay <raghav.upadhyay@tothenew.com>
2025-12-14 14:28:51 +0530

    project setup

---
+++ src/main/java/com/example/scyalladb/ScyalladbApplication.java
@@ -0,0 +1,13 @@
+package com.example.scyalladb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScyalladbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScyalladbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scyalladb/ScyalladbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scyalladb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScyalladbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
